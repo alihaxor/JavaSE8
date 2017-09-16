e1d3d83868fddd229c85ecf96d5ad226c869631c
Yusuf Ali <md.yousuf844@yahoo.com>
2017-09-17 04:49:27 +0600

    Parrallel Streams

---
+++ Java8NewFeauture/src/org/example/java8/ParallelStreams.java
@@ -0,0 +1,20 @@
+package org.example.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParallelStreams {
+    public static void main(String[] args) {
+
+        System.out.println("Creating List");
+        List<String> strings = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            strings.add("Item " + i);
+        }
+
+        strings.stream()
+                .parallel()
+                .forEach(str -> System.out.println(str));
+
+    }
+}
